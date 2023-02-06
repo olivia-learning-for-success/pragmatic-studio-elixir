d1d23cd6f9dfa00d599d9c8876e524774ba97a62
Olivia Dang <78009350+oliviadangpad@users.noreply.github.com>
2023-02-06 16:59:01 +1100

    7-8 pattern matching

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -5,13 +5,28 @@ defmodule Servy.Handler do
     # format_response(conv)
     request
     |> parse
+    |> rewrite_path
     |> log
     |> route
+    |> track
     |> format_response
   end
 
+  def track(%{status: 404, path: path} = conv) do
+    IO.puts("Warning: #{path} is on the loose")
+    conv
+  end
+  def track(conv), do: conv
+
+  def rewrite_path(%{path: "/wildlife"} = conv) do
+    %{conv | path: "/wildthings"}
+  end
+
+  def rewrite_path(conv), do: conv
+
   def log(conv), do: IO.inspect(conv)
 
+  @spec parse(binary) :: %{method: binary, path: binary, resp_body: <<>>, status: nil}
   def parse(request) do
     [method, path, _] =
       request
@@ -19,7 +34,11 @@ defmodule Servy.Handler do
       |> List.first()
       |> String.split(" ")
 
-    %{method: method, path: path, resp_body: ""}
+    %{method: method,
+    path: path,
+    resp_body: "",
+    status: nil,
+  }
   end
 
   @spec route(any) :: %{method: <<_::24>>, path: <<_::88>>, resp_body: <<_::160>>}
@@ -28,22 +47,40 @@ defmodule Servy.Handler do
   end
 
   def route(conv, "GET", "/wildthings") do
-    %{conv | resp_body: "Bears, Lions, Tigers"}
+    %{conv | status: 200, resp_body: "Bears, Lions, Tigers"}
   end
 
   def route(conv, "GET", "/bears") do
-    %{conv | resp_body: "Teddy Bears, Panda Bears, Koala Bears"}
+    %{conv | status: 200, resp_body: "Teddy Bears, Panda Bears, Koala Bears"}
+  end
+
+  def route(conv, "GET", "/bears/" <> id) do
+    %{conv | status: 200, resp_body: "Bear #{id}"}
+  end
+
+  def route(conv, _method, path) do
+    %{ conv | status: 404, resp_body: "404 Not Found, no #{path} here!"}
   end
 
   def format_response(conv) do
-    # TODO: Use values in the map to create an HTTP response string:
     """
-    HTTP/1.1 200 OK
+    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
     Content-Type: text/html
     Content-Length: #{String.length(conv.resp_body)}
 
     #{conv.resp_body}
     """
+  end
+
+  defp status_reason(code) do
+    %{
+      200 => "OK",
+      201 => "Created",
+      401 => "Unauthorized",
+      403 => "Forbidden",
+      404 => "Not Found",
+      500 => "Internal Server Error"
+    }[code]
   end
 end
 
@@ -71,6 +108,17 @@ IO.puts(response)
 
 request = """
 GET /bigfoot HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+IO.puts(response)
+
+request = """
+GET /bears/1 HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
